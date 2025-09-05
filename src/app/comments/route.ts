5a29e26c6ff89ea3a611ee6b2561da1bfae410c1
Shanto93 <shantoislam7363@gmail.com>
2025-09-05 14:51:13 +0600

    Headers in route handler

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,12 +1,26 @@
 import type { NextRequest } from "next/server";
 import { commentsData } from "./data";
+import { headers } from "next/headers";
 export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const query = searchParams.get("query");
-  const result = query
-    ? commentsData.filter((c) => c.comment.includes(query))
-    : commentsData;
-  return Response.json(result); return Response.json(commentsData);
+  // const searchParams = request.nextUrl.searchParams;
+  // const query = searchParams.get("search");
+  // const result = query
+  //   ? commentsData.filter((c) => c.comment.includes(query))
+  //   : commentsData;
+  // return Response.json(result); return Response.json(commentsData);
+
+  const headerList = await headers();
+  console.log(headerList.get("Authorization"));
+  // return Response.json(commentsData);
+  return new Response("<h1>Hello World</h1>", {
+    headers: {
+      "content-type": "text/html",
+    },
+  });
+
+  // const requestHeader = new Headers(request.headers);
+  // console.log(requestHeader.get("Authorization"));
+  // return Response.json(commentsData);
 }
 
 export async function POST(request: Request) {
@@ -15,7 +29,7 @@ export async function POST(request: Request) {
     id: commentsData.length + 1,
     comment: newComment.comment,
   };
-  commentsData.push(updated); 
+  commentsData.push(updated);
   return new Response(JSON.stringify(updated), {
     headers: { "Content-Type": "application/json" },
     status: 201,
